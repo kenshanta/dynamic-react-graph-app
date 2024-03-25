37ea583fa01d6e4ebb4786ade9347ac5f392d6a0
Sarkis Kovlekjian <kovlekjian@gmail.com>
2024-03-26 00:55:53 +0100

    resolve deployment issue caused by import

--- src/components/customChart.tsx
+++ src/components/customChart.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { ChartType } from "chartjs";
 import { Chart } from "react-chartjs-2";
 import { Box } from "@mui/material";
 
 interface Datasets {
-  type: ChartType;
+  type: any;
   label: string;
   borderColor: string;
   borderWidth: number;
